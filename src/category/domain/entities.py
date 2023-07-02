df15a48a2b028bc2b9a4ed7b4ab585d6dbeae104
Alex Cândido <alex.candido.tec@gmail.com>
2023-07-01 21:57:53 -0300

    Teste Verificando se classe implementa um dataclass

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from typing import Optional
 
 
-@dataclass()
+@dataclass() #init, repr, eq
 class Category:
 
   name: str
@@ -14,3 +14,17 @@ class Category:
       default_factory=lambda: datetime.now()
     )
   
+# piramide de testes
+# testes de unidades
+# testes de integração
+# testes e2e
+
+# Michael Feathers
+
+
+# POST /categories - controller - usecase -> { name: "256" , invalidas} -> entidade
+
+# Notification Pattern - Martin Fowler
+# lib - Limite Parcial da Arquitetura
+
+# required, max, positive, negativo, email, uuid, cartao de credito
