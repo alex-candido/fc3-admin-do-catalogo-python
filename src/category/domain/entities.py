657f6eb282f5df861915a8447b83d3dea4d6ae32
Alex Cândido <alex.candido.tec@gmail.com>
2023-08-06 20:43:18 -0300

    Adicionando ID na entidade Categoria e sobre kw_only no Data Class

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,12 +1,15 @@
 
+import uuid
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional
 
 
-@dataclass() #init, repr, eq
+@dataclass(kw_only=True, frozen=True, slots=True) #init, repr, eq
 class Category:
-
+  id: uuid.UUID = field(
+      default_factory=lambda: uuid.uuid4()
+    )
   name: str
   description: Optional[str] = None
   is_active: Optional[bool] = True
