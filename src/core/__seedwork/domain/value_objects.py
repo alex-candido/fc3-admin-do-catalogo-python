f0b56a24c6e11179c1339e92129e794f618c2af5
alex-candido <alex.candido.tec@gmail.com>
2024-08-04 15:43:03 -0300

    Criando abstração para um Value Object

--- src/core/__seedwork/domain/value_objects.py
+++ src/core/__seedwork/domain/value_objects.py
@@ -1,10 +1,22 @@
 import uuid
-from dataclasses import dataclass, field
+import json
+
+from abc import ABC
+from dataclasses import dataclass, field, fields
 
 from core.__seedwork.domain.exceptions import InvalidUuidException
 
 @dataclass(frozen=True, slots=True)
-class UniqueEntityId:
+class ValueObject(ABC):
+    def __str__(self) -> str:
+        fields_name = [field.name for field in fields(self)]
+        print(len(fields_name))
+        return str(getattr(self, fields_name[0])) \
+            if len(fields_name) == 1 \
+            else json.dumps({field_name: getattr(self, field_name) for field_name in fields_name})
+
+@dataclass(frozen=True, slots=True)
+class UniqueEntityId(ValueObject):
     id: str = field(  # pylint: disable=invalid-name
         default_factory=lambda: str(uuid.uuid4())
     )
