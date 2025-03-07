0ef9ba59c7d420d92e6ca754de8039bf0531a309
Alex Candido <alex.candido.tec@gmail.com>
2025-03-07 13:55:50 -0300

    criado estrutura de validações para entidade

--- src/core/category/domain/entities.py
+++ src/core/category/domain/entities.py
@@ -3,15 +3,35 @@ from datetime import datetime
 from typing import Optional
 
 from core.__seedwork.domain.entities import Entity
+from core.__seedwork.domain.exceptions import EntityValidationException
 
-@dataclass(kw_only=True, frozen=True, slots=True) #init, repr, eq
+from core.category.domain.validators import CategoryValidatorFactory
+
+@dataclass(kw_only=True, frozen=True, slots=True)  # init, repr, eq
 class Category(Entity):
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
     created_at: Optional[datetime] = field(
         default_factory=lambda: datetime.now()
-      )
+    )
+    
+    def update(self, name: str, description: str):
+        self._set("name", name)
+        self._set("description", description)
+        self.validate()
+
+    def activate(self):
+        self._set("is_active", True)
+        
+    def deactivate(self):
+        self._set("is_active", False)
+        
+    def validate(self):
+        validator = CategoryValidatorFactory.create()
+        is_valid = validator.validate(self.to_dict())
+        if not is_valid:
+            raise EntityValidationException(validator.errors)
 
 # piramide de testes
 # testes de unidades
@@ -21,7 +41,8 @@ class Category(Entity):
 # Michael Feathers
 
 
-# POST /categories - controller - usecase -> { name: "256" , invalidas} -> entidade
+# POST /categories - controller - usecase -> { name: "256" , invalidas} ->
+# entidade
 
 # Notification Pattern - Martin Fowler
 # lib - Limite Parcial da Arquitetura
