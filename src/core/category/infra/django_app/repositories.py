3d3f8bb8e16d90c1ec1e794d735e5cdc5f5f4c1e
Alex Candido <alex.candido.tec@gmail.com>
2025-03-13 13:24:23 -0300

    criando interface para repositórios com buscas personalizadas

---
+++ src/core/category/infra/django_app/repositories.py
@@ -0,0 +1,17 @@
+# pylint: disable=no-member,unexpected-keyword-arg
+from typing import List, Type, TYPE_CHECKING
+from django.core import exceptions as django_exceptions
+from django.core.paginator import Paginator
+from core.__seedwork.domain.exceptions import NotFoundException
+from core.__seedwork.domain.value_objects import UniqueEntityId
+from core.category.domain.entities import Category
+from core.category.domain.repositories import CategoryRepository
+from core.category.infra.django_app.mappers import CategoryModelMapper
+
+
+if TYPE_CHECKING:
+    from core.category.infra.django_app.models import CategoryModel
+
+
+class CategoryDjangoRepository(CategoryRepository):
+    pass
