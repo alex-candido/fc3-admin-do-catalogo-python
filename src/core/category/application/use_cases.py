fd9178ffaa27c0dd081952df826b03aaaadc457c
Alex Candido <alex.candido.tec@gmail.com>
2025-03-14 11:58:48 -0300

    criando mappers para gerar outputs

--- src/core/category/application/use_cases.py
+++ src/core/category/application/use_cases.py
@@ -8,58 +8,53 @@ from core.category.domain.entities import Category
 from core.category.domain.repositories import CategoryRepository
 from core.category.application.dto import CategoryOutput, CategoryOutputMapper
 
+
 @dataclass(slots=True, frozen=True)
-class CreateCategoryUseCase():
-    
+class CreateCategoryUseCase(UseCase):
+
     category_repo: CategoryRepository
-    
+
     def execute(self, input_param: 'Input') -> 'Output':
         category = Category(
             name=input_param.name,
             description=input_param.description,
             is_active=input_param.is_active
         )
-        
         self.category_repo.insert(category)
-        return self.Output(
-            id=category.id,
-            name=category.name,
-            description=category.description,
-            is_activate=category.is_active,
-            created_at=category.created_at
-        )
-    
+        return self.__to_output(category)
+
+    def __to_output(self, category: Category):  # pylint: disable=no-self-use
+        return CategoryOutputMapper\
+            .from_child(CreateCategoryUseCase.Output)\
+            .to_output(category)
+
     @dataclass(slots=True, frozen=True)
     class Input:
         name: str
-        description: Optional[str]
-        is_active: Optional[bool]
-        
+        description: Optional[str] = Category.get_field('description').default
+        is_active: Optional[bool] = Category.get_field('is_active').default
+
     @dataclass(slots=True, frozen=True)
     class Output(CategoryOutput):
         pass
-        
+
 
 @dataclass(slots=True, frozen=True)
 class GetCategoryUseCase(UseCase):
-    
+
     category_repo: CategoryRepository
-    
-    def execute(self, input_param: 'input') -> 'Output':
+
+    def execute(self, input_param: 'Input') -> 'Output':
         category = self.category_repo.find_by_id(input_param.id)
-        return self.Output(
-            id=category.id,
-            name=category.name,
-            description=category.description,
-            is_activate=category.is_active,
-            created_at=category.created_at
-        )
+        return self.__to_output(category)
+
+    def __to_output(self, category: Category):  # pylint: disable=no-self-use
+        return CategoryOutputMapper.from_child(GetCategoryUseCase.Output).to_output(category)
 
     @dataclass(slots=True, frozen=True)
     class Input:
-        id: str
-        
-    @dataclass(slots=True, frozen=True)
+        id: str  # pylint: disable=invalid-name
+
     @dataclass(slots=True, frozen=True)
     class Output(CategoryOutput):
         pass
