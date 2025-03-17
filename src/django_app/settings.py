f551a78a03e221ea69bdc176300555119857621f
Alex Candido <alex.candido.tec@gmail.com>
2025-03-17 17:44:40 -0300

    criando model category

--- src/django_app/settings.py
+++ src/django_app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*q3f66_(ii3udrgg$!m$gjh**c&&@5mo*j@3=@@nrkvfkv&5)c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'core.category.infra.django_app',
 ]
 
 MIDDLEWARE = [
