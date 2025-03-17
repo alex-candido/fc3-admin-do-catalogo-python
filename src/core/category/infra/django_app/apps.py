38f77195ab261e764a1ca00a72dfaec8de0a61ce
Alex Candido <alex.candido.tec@gmail.com>
2025-03-17 17:44:40 -0300

    criando model category

--- src/core/category/infra/django_app/apps.py
+++ src/core/category/infra/django_app/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class CategoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'category'
+    name = 'core.category.infra.django_app'
+    label = 'category'
+    verbose_name = 'Categorias'
