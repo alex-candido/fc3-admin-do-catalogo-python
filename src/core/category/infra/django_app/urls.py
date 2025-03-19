dfa7429548d9080f4ff3dd64719caff026c2e2a5
Alex Candido <alex.candido.tec@gmail.com>
2025-03-18 22:37:12 -0300

    usando container de servicos no Django

--- src/core/category/infra/django_app/urls.py
+++ src/core/category/infra/django_app/urls.py
@@ -1,32 +1,24 @@
 
-from dataclasses import dataclass
 from django.urls import path  # pylint: disable=import-self
-from core.category.application.use_cases import CreateCategoryUseCase
-from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
 from django_app import container
 from .api import CategoryResource
 
-@dataclass()
-class CategoryInMemoryRepositoryFactory:
 
-    repo: CategoryInMemoryRepository = None
+def __init_category_resource():
+    return {
+        'create_use_case': container.use_case_category_create_category,
+        'list_use_case': container.use_case_category_list_categories,
+        'get_use_case': container.use_case_category_get_category,
+        'update_use_case': container.use_case_category_update_category,
+        'delete_use_case': container.use_case_category_delete_category,
+    }
 
-    @classmethod
-    def create(cls):
-      if not cls.repo:
-        cls.repo = CategoryInMemoryRepository()
-      return cls.repo
-
-class CreateCategoryUseCaseFactory:
-
-    @classmethod
-    def create(cls):
-      repo = CategoryInMemoryRepositoryFactory.create()
-      return CreateCategoryUseCase(repo)
 
 urlpatterns = [
     path('categories/', CategoryResource.as_view(
-        create_use_case=CreateCategoryUseCaseFactory.create())
-    ),
-    path('categories/<id>/', CategoryResource.as_view())
+        **__init_category_resource()
+    )),
+    path('categories/<id>/', CategoryResource.as_view(
+        **__init_category_resource()
+    )),
 ]
