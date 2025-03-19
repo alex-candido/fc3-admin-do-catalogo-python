b9c580ce722d93ae68f20d8be57e979e057ea3a1
Alex Candido <alex.candido.tec@gmail.com>
2025-03-19 16:48:01 -0300

    criando API Rest de Categoria In Memory

--- src/core/category/infra/django_app/api.py
+++ src/core/category/infra/django_app/api.py
@@ -26,13 +26,30 @@ class CategoryResource(APIView):
     update_use_case: Callable[[], UpdateCategoryUseCase]
     delete_use_case: Callable[[], DeleteCategoryUseCase]
 
-    def post(self, request: Request):
+    def post(self, request: DrfRequest):
       input_param = CreateCategoryUseCase.Input(name=request.data['name'])
       output = self.create_use_case().execute(input_param)
       return Response(asdict(output), status=http.HTTP_201_CREATED)
 
-    def get(self, request: Request):
+    def get(self, request: DrfRequest, id: str = None):
+      if id:
+        return self.get_object(id)
       input_param = ListCategoriesUseCase.Input(**request.query_params.dict())
       output = self.list_use_case().execute(input_param)
       return Response(asdict(output))
+
+    def get_object(self, id: str):
+      input_param = GetCategoryUseCase.Input(id=id)
+      output = self.get_use_case().execute(input_param)
+      return Response(asdict(output))
+
+    def put(self, request: DrfRequest, id: str):
+      input_param = UpdateCategoryUseCase.Input(**{'id': id, **request.data})
+      output = self.update_use_case().execute(input_param)
+      return Response(asdict(output))
+
+    def delete(self, _request: DrfRequest, id: str):
+      input_param = DeleteCategoryUseCase.Input(id=id)
+      self.delete_use_case().execute(input_param)
+      return Response(status=http.HTTP_204_NO_CONTENT)
 
