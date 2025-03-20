49acbddb7a06e807b3ba732414060c13d9522a02
Alex Candido <alex.candido.tec@gmail.com>
2025-03-20 17:45:05 -0300

    integração com o banco de dados django orm

--- src/core/category/infra/django_app/repositories.py
+++ src/core/category/infra/django_app/repositories.py
@@ -13,4 +13,31 @@ if TYPE_CHECKING:
 
 
 class CategoryDjangoRepository(CategoryRepository):
-    pass
+    
+    def insert(self, category: Category) -> Category:
+        CategoryModel.objects.create(**category.to_dict())
+    
+    def find_by_id(self, entity_id: str | UniqueEntityId) -> Category:
+        id_str = str(entity_id)
+        model = self._get(id_str)
+        return Category(unique_entity_id=UniqueEntityId(id_str), **model.to_dict())
+    
+    def find_all(self) -> List[Category]:
+        raise NotImplementedError()
+    
+    def update(self, category: Category) -> None:
+        raise NotImplementedError()
+    
+    def delete(self, entity_id: str | UniqueEntityId) -> None:
+        raise NotImplementedError()
+    
+    def _get(self, entity_id: str) -> 'CategoryModel':
+        try:
+            return CategoryModel.objects.get(pk=entity_id)
+        except (CategoryModel.DoesNotExist, django_exceptions.ValidationError) as exception:
+            raise NotFoundException(
+                f"Entity not found using ID '{entity_id}'") from exception
+    
+    def search(self, input_params: CategoryRepository.SearchParams) -> CategoryRepository.SearchResult:
+        raise NotImplementedError()
+    
