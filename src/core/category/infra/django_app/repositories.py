4695df69a732c2aa0455d190abd5dd4d20d28dd7
Alex Candido <alex.candido.tec@gmail.com>
2025-03-20 22:00:59 -0300

    criando mapeador de entidade para model

--- src/core/category/infra/django_app/repositories.py
+++ src/core/category/infra/django_app/repositories.py
@@ -6,6 +6,7 @@ from core.__seedwork.domain.exceptions import NotFoundException
 from core.__seedwork.domain.value_objects import UniqueEntityId
 from core.category.domain.entities import Category
 from core.category.domain.repositories import CategoryRepository
+from core.category.infra.django_app.mappers import CategoryModelMapper
 
 
 if TYPE_CHECKING:
@@ -13,31 +14,43 @@ if TYPE_CHECKING:
 
 
 class CategoryDjangoRepository(CategoryRepository):
-    
-    def insert(self, category: Category) -> Category:
-        CategoryModel.objects.create(**category.to_dict())
-    
+
+    sortable_fields: List[str] = ['name', 'created_at']
+    model: Type['CategoryModel']
+
+    def __init__(self) -> None:
+        from core.category.infra.django_app.models import CategoryModel  # pylint: disable=import-outside-toplevel
+        self.model = CategoryModel
+
+    def insert(self, entity: Category) -> None:
+        model = CategoryModelMapper.to_model(entity)
+        model.save()
+
     def find_by_id(self, entity_id: str | UniqueEntityId) -> Category:
         id_str = str(entity_id)
         model = self._get(id_str)
-        return Category(unique_entity_id=UniqueEntityId(id_str), **model.to_dict())
-    
+        return CategoryModelMapper.to_entity(model)
+
     def find_all(self) -> List[Category]:
-        raise NotImplementedError()
-    
-    def update(self, category: Category) -> None:
-        raise NotImplementedError()
-    
+        return [CategoryModelMapper.to_entity(model) for model in self.model.objects.all()]
+
+    def update(self, entity: Category) -> None:
+        self._get(entity.id)
+        model = CategoryModelMapper.to_model(entity)
+        model.save()
+
     def delete(self, entity_id: str | UniqueEntityId) -> None:
-        raise NotImplementedError()
-    
+        id_str = str(entity_id)
+        model = self._get(id_str)
+        model.delete()
+
     def _get(self, entity_id: str) -> 'CategoryModel':
         try:
-            return CategoryModel.objects.get(pk=entity_id)
-        except (CategoryModel.DoesNotExist, django_exceptions.ValidationError) as exception:
+            return self.model.objects.get(pk=entity_id)
+        except (self.model.DoesNotExist, django_exceptions.ValidationError) as exception:
             raise NotFoundException(
                 f"Entity not found using ID '{entity_id}'") from exception
-    
+
     def search(self, input_params: CategoryRepository.SearchParams) -> CategoryRepository.SearchResult:
         raise NotImplementedError()
-    
+
