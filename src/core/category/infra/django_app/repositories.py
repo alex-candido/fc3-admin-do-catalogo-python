8bcb4b0731426bc1c7453512b7519d574bc72b87
Alex Candido <alex.candido.tec@gmail.com>
2025-03-20 22:58:21 -0300

    implementado metodos insert, find_by_id, find_all, update, delete e search em repositories

--- src/core/category/infra/django_app/repositories.py
+++ src/core/category/infra/django_app/repositories.py
@@ -26,6 +26,15 @@ class CategoryDjangoRepository(CategoryRepository):
         model = CategoryModelMapper.to_model(entity)
         model.save()
 
+    def bulk_insert(self, entities: List[Category]) -> None:
+        self.model.objects.bulk_create(
+            list(
+                map(
+                    CategoryModelMapper.to_model, entities
+                )
+            )
+        )
+
     def find_by_id(self, entity_id: str | UniqueEntityId) -> Category:
         id_str = str(entity_id)
         model = self._get(id_str)
@@ -52,5 +61,27 @@ class CategoryDjangoRepository(CategoryRepository):
                 f"Entity not found using ID '{entity_id}'") from exception
 
     def search(self, input_params: CategoryRepository.SearchParams) -> CategoryRepository.SearchResult:
-        raise NotImplementedError()
+        query = self.model.objects.all()
 
+        if input_params.filter:
+            query = query.filter(name__icontains=input_params.filter)
+        if input_params.sort and input_params.sort in self.sortable_fields:
+            query = query.order_by(
+                input_params.sort if input_params.sort_dir == 'asc' else f'-{input_params.sort}'
+            )
+        else:
+            query = query.order_by('-created_at')
+
+        paginator = Paginator(query, input_params.per_page)
+        page_obj = paginator.page(input_params.page)
+
+        return CategoryRepository.SearchResult(
+            items=[CategoryModelMapper.to_entity(
+                model) for model in page_obj.object_list],
+            total=paginator.count,
+            current_page=input_params.page,
+            per_page=input_params.per_page,
+            sort=input_params.sort,
+            sort_dir=input_params.sort_dir,
+            filter=input_params.filter
+        )
